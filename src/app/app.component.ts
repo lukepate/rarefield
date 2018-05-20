b5d1f360ffe05a19934d67d78bf5c22dc5b1a919
Luke pate <hello@weareany.com>
2018-05-20 11:21:10 -0400

    added space to header ul

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  mic: any = true;
+  hard: any = false;
+  soft: any = false;
+  Hardware: any;
+  Software: any;
+
+  changeMic(){
+    if(this.mic){
+      this.mic = true;
+    } else{
+      this.mic = true;
+      this.hard = false;
+      this.soft = false;
+    }
+  }
+
+  changeHard(){
+    if(this.hard){
+      this.hard = false;
+      this.mic = true;
+    } else{
+      this.hard = true;
+      this.mic = false;
+      this.soft = false;
+    }
+  }
+
+  changeSoft(){
+    if(this.soft){
+      this.mic = true;
+    } else{
+      this.soft = true;
+      this.hard = false;
+      this.mic = false;
+    }
+  }
+
+
+
+
 }
